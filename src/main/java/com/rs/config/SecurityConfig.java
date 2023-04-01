1dcd6a6bbf25456a40ba4c3fb51f41250d06ae32
Sagir <sagir.md@sil.mu>
2023-04-01 16:31:32 +0400

    setting for the opteque token

--- src/main/java/com/rs/config/SecurityConfig.java
+++ src/main/java/com/rs/config/SecurityConfig.java
@@ -8,12 +8,25 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 public class SecurityConfig {
 
-    @Bean
+    /*@Bean
     public SecurityFilterChain securityFilterChain( HttpSecurity httpSecurity) throws Exception {
 
         httpSecurity.oauth2ResourceServer(r -> r.jwt().jwkSetUri("http://localhost:8080/oauth2/jwks")
         .jwtAuthenticationConverter(new CustomJwtAuthenticationTokenConverter()));
         httpSecurity.authorizeHttpRequests().anyRequest().authenticated();
         return httpSecurity.build();
+    }*/
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.oauth2ResourceServer()
+                .opaqueToken()
+                .introspectionUri("http://localhost:8080/oauth2/introspect")
+                .introspectionClientCredentials("client" ,"secret");
+
+        http.authorizeHttpRequests()
+                .anyRequest().authenticated();
+
+        return http.build();
     }
 }
